c33d705164e3a8fdf3975da081325a50d6cd8bae
Orifjon-beep <orifjonotkirov@gmail.com>
2025-11-26 13:44:24 +0500

    Remove headless option from Chrome browser setup

--- conftest.py
+++ conftest.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import pytest
 
 
@@ -8,9 +7,7 @@ import pytest
 
 @pytest.fixture()
 def browser():
-    options = Options()
-    options.add_argument('--headless')
-    browser = webdriver.Chrome(options=options)
+    browser = webdriver.Chrome()
     browser.implicitly_wait(5)
     yield browser
     browser.close()
