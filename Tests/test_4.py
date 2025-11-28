6802c7ba948b06f8053d52660b4daabc498887ea
Orifjon-beep <orifjonotkirov@gmail.com>
2025-11-28 12:11:50 +0500

    Update test_4.py

--- Tests/test_4.py
+++ Tests/test_4.py
@@ -78,16 +78,16 @@ from pages.homepage import HomePage
 #     homepage.button_11()
 #     homepage.check_title_is('Reviews')
 
-def test_complaints(browser):
-    homepage = HomePage(browser)
-    homepage.open()
-    homepage.sumbit()
-    homepage.button_12()
-    homepage.check_title_is('Complaints')
-
-# def test_reports(browser):
+# def test_complaints(browser):
 #     homepage = HomePage(browser)
 #     homepage.open()
 #     homepage.sumbit()
-#     homepage.button_13()
-#     homepage.check_title_is('Reports')
+#     homepage.button_12()
+#     homepage.check_title_is('Complaints')
+
+def test_reports(browser):
+    homepage = HomePage(browser)
+    homepage.open()
+    homepage.sumbit()
+    homepage.button_13()
+    homepage.check_title_is('Reports')
